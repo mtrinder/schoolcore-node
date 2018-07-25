2f0ecf749802faf9b8ed7f536e060d1e5c36d7e3
Mark Trinder <marktrinder@hotmail.com>
2018-07-25 19:40:31 +1000

    Ports

--- benchmarks/index.js
+++ benchmarks/index.js
@@ -52,7 +52,7 @@ bitcoind.on('ready', function() {
 
   var client = new bitcoin.Client({
     host: 'localhost',
-    port: 19332,
+    port: 18442,
     user: 'bitcoin',
     pass: 'local321'
   });
--- regtest/bitcoind.js
+++ regtest/bitcoind.js
@@ -442,7 +442,7 @@ describe('Bitcoind Functionality', function() {
         tx.outputSatoshis.should.equal(50 * 1e8);
         tx.inputSatoshis.should.equal(0);
         tx.inputs.length.should.equal(1);
-        tx.outputs.length.should.equal(2); // segwit
+        tx.outputs.length.should.equal(1);
         should.equal(tx.inputs[0].prevTxId, null);
         should.equal(tx.inputs[0].outputIndex, null);
         tx.inputs[0].script.should.be.a('string');
